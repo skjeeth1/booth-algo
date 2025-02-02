6cf6d74b5a335ea407fac9e83d16fc600d3c0c0a
skjeeth1 <skjeeth1@gmail.com>
2025-02-02 13:46:37 +0530

    Modified code

--- main.c
+++ main.c
@@ -1,35 +1,61 @@
 #include <stdio.h>
 
+int booth(int a, int b)
+{   
+    // Logic:
+    // All operations are done in b itself
+    // Here both A & Q register vals are stored in b
+    // Q-1 is stored in rem
+    // The function returns the value of b
+    // The variable b is split into two, with the first 
+    // half being the A register and the second half being the Q register
+
+    int rem = 0; // Q-1 bit
+    int num_size = 16; // Size of the registers
+    if (b < 0) b += 1 << num_size;
+
+    for (int i = sizeof(int) * 8 / 2; i > 0; i--)
+    {   
+        // Check if the last two bits are 01 or 10
+        if ((b & 1) && !rem) {
+            // A - M if Q0Q-1 = 10
+            b = b + (-a << num_size);
+        } else if (!(b & 1) && rem) {
+            // A + M if Q0Q-1 = 01
+            b = b + (a << num_size); 
+        }
+        
+        // Get Q-1
+        rem = b & 1;
+        // Right shift b
+        b = b >> 1;
+    }
+
+    return b;
+}
+
 int main() 
 {
     int a = 0;
     int b = 0;
 
-    printf("Enter a: ");
+    printf("Enter a (|a|<32768): ");
     scanf("%d",&a);
 
-    printf("Enter b: ");
+    printf("Enter b (|b|<32768): ");
     scanf("%d",&b);
-    
-    int expected_val = a * b;
 
-    int rem = 0;
-    int num_size = 16;
-    if (b < 0) b += 1 << num_size;
-
-    for (int i = sizeof(int) * 8 / 2; i > 0; i--)
-    {   
-        if ((b & 1) && !rem) {
-            b = b + (-a << num_size);
-        } else if (!(b & 1) && rem) {
-            b = b + (a << num_size); 
-        }
-        
-        rem = b & 1;
-        b = b >> 1;
+    // Since each number is 16 bit long,
+    // Magnitude of operand cannot be greater than 2^15
+    if (a >= 32768 || b >= 32768 || a <= -32768 || b <= -32768) {
+        printf("Please enter numbers less than 2^15\n");
+        return -1;
     }
 
-    printf("Answer by Booth's Algorithm: %d\n", b);
+    int expected_val = a * b;
+    int booths_val = booth(a, b);
+
+    printf("Answer by Booth's Algorithm: %d\n", booths_val);
     printf("Actual answer: %d\n", expected_val);
 
 }
